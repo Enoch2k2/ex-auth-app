0028bd7e352be8b34d896c7a392aede24191690a
Enoch2k2 <enoch2k2@hotmail.com>
2019-05-01 15:52:01 -0500

    updated proxy

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -1,7 +1,7 @@
 export const loadUsers = () => {
   return dispatch => {
     dispatch({type: "LOADING"});
-    return fetch('/users')
+    return fetch('/api/v1/users')
       .then(resp => resp.json())
       .then(users => dispatch({type: "GET_USERS", payload: users}))
   }
